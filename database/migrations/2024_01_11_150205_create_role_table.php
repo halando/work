6f18ef7ba2a84a477bdbf18d4032eead443c3fa2
Andó Attila <andoattila698@gmail.com>
2024-01-11 16:34:14 +0100

    tr

---
+++ database/migrations/2024_01_11_150205_create_role_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('role', function (Blueprint $table) {
+            $table->id();
+            $table->string("Esztergályos");
+            $table->string("Művész");
+            $table->string("Kőműves");
+            $table->string("Bankár");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('role');
+    }
+};
